cb8436f240e3db998a02b818ac988c6cac6e8b3e
ihollander <ianholla@gmail.com>
2021-07-09 17:12:48 -0400

    Solution

--- contacts.rb
+++ contacts.rb
@@ -18,7 +18,15 @@ def contacts
 end
 
 def remove_strawberry(contacts)
-  # your code here!
+  contacts.each do |person, contact_details_hash|
+    if person == "Freddy Mercury"
+      contact_details_hash.each do |attribute, data|
+        if attribute == :favorite_ice_cream_flavors
+          data.delete_if {|ice_cream| ice_cream == "strawberry"}
+        end
+      end
+    end
+  end
 end
 
 # print the output to the terminal for inspection
